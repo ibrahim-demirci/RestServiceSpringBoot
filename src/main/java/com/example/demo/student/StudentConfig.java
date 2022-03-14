9377ef3c5a4cecbd7ab98657292dd093c322eeae
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-03-14 23:01:01 +0300

    Create StudentConfig file and add two Student to database

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo.student;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.AUGUST;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "mariam@gmail.com",
+                    LocalDate.of(2000, AUGUST, 30),
+                    21
+            );
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, AUGUST, 30),
+                    21
+            );
+
+            repository.saveAll(List.of(mariam,alex));
+
+        };
+
+    }
+}
