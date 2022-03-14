8a1945cda77684d074e0e73e5ee35a5965ac2db2
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-03-14 12:54:31 +0300

    Create StudentController and add path to get Students

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,36 +1,20 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
+
 
 @SpringBootApplication
 // Make this class to serve rest endpoints
-@RestController
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(
-				new Student(
-						1L,
-						"Mariam",
-						"mariam@gmail.com",
-						LocalDate.of(2000, Month.AUGUST,30),
-						21
-				)
-		);
-	}
+
 
 
 }
