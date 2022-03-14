e6ddc5ee62c6ac72144eb22f45a117d8c8623c3d
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-03-14 13:28:57 +0300

    Dependecny Injection

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,11 +1,15 @@
 package com.example.demo.student;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service
 public class StudentService {
-    
+
     public List<Student> getStudents() {
         return List.of(
                 new Student(
